c7626a8ef756c79952f1199545bbf4c6db189f61
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-19 10:08:58 -0400

    starting

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+it('sets the location HREF', () => {
+  cy.visit('index.html')
+  cy.contains('ACME Laboratories')
+})
