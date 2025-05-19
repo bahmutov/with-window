13ea1a32d74a8f0b248f47892eaccf39ba34b2f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-19 10:15:01 -0400

    separate script

---
+++ app.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  console.log('changing window location to acme.com')
+  location.href = 'https://acme.com'
+}, 1000)
--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,13 @@ module.exports = defineConfig({
     // baseUrl, etc
     supportFile: false,
     fixturesFolder: false,
+    blockHosts: [
+      'pagead2.googlesyndication.com',
+      'ad.doubleclick.net',
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // and load any plugins that require the Node environment
     },
-  }
+  },
 })
